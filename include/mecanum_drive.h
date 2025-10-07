a778eba640a64dcbeba1f823651e2970a168fecb
Quang Doan <quang.m.doan@gmail.com>
2025-10-06 17:58:56 -0700

    Cleanup

--- include/mecanum_drive.h
+++ include/mecanum_drive.h
@@ -5,7 +5,6 @@
 
 #include "lemlib/api.hpp" // IWYU pragma: keep
 
-
 class MecanumDriveTrain {
 public: 
   double WheelDiameter = 2.0* INCH2METERS; 
@@ -17,10 +16,10 @@ public:
   double frontRightVelocity;
   double backRightVelocity;
 
-  MecanumDriveTrain(): frontLeft(12),
-                       backLeft(11), 
-                       frontRight(-20),
-                       backRight(-19){}
+  MecanumDriveTrain(): frontLeftMotor(12),
+                       backLeftMotor(11), 
+                       frontRightMotor(-20),
+                       backRightMotor(-19){}
 
 
   void arcadeDrive(double a3, double a1, double a4) {
@@ -29,17 +28,17 @@ public:
     frontRightVelocity = (a3 - a1 - a4) / 2; 
     backRightVelocity  = (a3 - a1 + a4) / 2;
 
-    frontLeft.move_velocity(frontLeftVelocity);
-    backLeft.move_velocity(backLeftVelocity);
-    frontRight.move_velocity(frontRightVelocity);
-    backRight.move_velocity(backRightVelocity);
+    frontLeftMotor.move_velocity(frontLeftVelocity);
+    backLeftMotor.move_velocity(backLeftVelocity);
+    frontRightMotor.move_velocity(frontRightVelocity);
+    backRightMotor.move_velocity(backRightVelocity);
   }
 
 private: 
-  pros::Motor frontLeft;
-  pros::Motor frontRight;
+  pros::Motor frontLeftMotor;
+  pros::Motor frontRightMotor;
 
-  pros::Motor backLeft;
-  pros::Motor backRight;
+  pros::Motor backLeftMotor;
+  pros::Motor backRightMotor;
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 
 MecanumDriveTrain driveTrain;
 
-pros::Motor IntakeMotor(6);
-
 // initialize function. Runs on program startup
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
@@ -13,15 +11,16 @@ void initialize() {
     // print position to brain screen
     pros::Task screen_task([&]() {
         while (true) {
-            pros::lcd::print(0, "frontLeftVelocity: %f", driveTrain.frontLeftVelocity); 
+            pros::lcd::print(1, "frontLeftVelocity: %.3f", driveTrain.frontLeftVelocity); 
+            pros::lcd::print(3, "backLeftVelocity: %.3f", driveTrain.backLeftVelocity); 
+            pros::lcd::print(5, "frontRightVelocity: %.3f", driveTrain.frontRightVelocity); 
+            pros::lcd::print(7, "backRightVelocity: %.3f", driveTrain.backRightVelocity); 
+            
             // delay to save resources
             pros::delay(20);
         }
     });
 }
-
-
-
 
 void opcontrol() {
     // loop forever
@@ -31,13 +30,12 @@ void opcontrol() {
         // Axis2 - Up and down of the right joystick.
         // Axis3 - Up and down of the left joystick.
         // Axis4 - Left and right of the left joystick.
-        // autoDrive.arcadeDrive(Controller.Axis3.value(), Controller.Axis1.value(), Controller.Axis4.value());
+        // driveTrain.arcadeDrive(Controller.Axis3.value(), Controller.Axis1.value(), Controller.Axis4.value());
   
         // move the robot
         double a3 = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         double a1 = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
         double a4 = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
-
         driveTrain.arcadeDrive(a3, a1, a4);
 
         // delay to save resources
